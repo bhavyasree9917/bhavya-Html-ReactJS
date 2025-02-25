7f12d55c03f1419fcd5d3af773f0aef5949292e0
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-02-25 21:10:50 +0530

    react-js

--- REACTJS/app2-intro/src/App.js
+++ REACTJS/app2-intro/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
- <h2>konidela pawankalyan</h2>             
+ <h2>konidela pawankalyan</h2>   
+           
     </div>
   );
 }
