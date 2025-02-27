d5626d1b9558698c7324c9388b1dd8b8a573642e
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-02-27 20:37:56 +0530

    react-js

--- REACTJS/app2-intro/src/App.js
+++ REACTJS/app2-intro/src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
- <h2>konidela pawankalyan</h2>   
+ <h2>reactjs projects</h2>
+    <p>application form for student enrollment</p>
+    <label for="">First Name:</label>
+          <input type="text" />
+        
+          <label for="">Last Name:</label>
+          <input type="text" />
+          
+          <label for="">DateOfBirth: </label>
+          <input type="date"  />
+          
+          <label for="">Email ID: </label>
+          <input type="text" readonly />
+          
+          <label for="">Mobilenumber: </label>
+          <input type="number" /> 
            
     </div>
   );
