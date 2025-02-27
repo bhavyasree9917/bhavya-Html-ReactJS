7c3069daa411921a1a5c30721a076b4f444fa5a6
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-02-27 20:46:03 +0530

    react-js

--- REACTJS/app3-intro/src/App.js
+++ REACTJS/app3-intro/src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h2>sriramula bhavyasree</h2>
+      <h2>konidela pawankalyan</h2>
+      <p>andrapradesh deputy cm pawankalyan garu</p>
+      <ul>
+        <li>pawan</li>
+        <li>kalyan</li>
+        <li>ram</li>
+        <li>sree</li>
+      </ul>
+      
     </div>
   );
 }
