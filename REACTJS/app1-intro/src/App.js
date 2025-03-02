96b5f58620ba31ae44ec520fca5e8bb6500086ad
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-02 20:33:10 +0530

    react-js

--- REACTJS/app1-intro/src/App.js
+++ REACTJS/app1-intro/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Bhavyasree from './Bhavyasree';
+import Ram from './Ram';
+import Ramesh from './Ramesh';
+import Premtej from './Premtej';
+
 
 function App() {
   return (
     <div className="App">
-      <h2>this is react js</h2>
-      <p>introduction to javascript</p>
+    <Bhavyasree/>
+    <hr/>
+    <Ram/>
+    <Ramesh/>
+    <Premtej/>
     </div>
   );
   
