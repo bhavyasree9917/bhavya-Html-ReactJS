49a0302cc7c07781da0e06b1b849cb67e02e0365
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-02 15:36:22 +0530

    react-js

---
+++ REACTJS/app15 class components-intro/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Bhavyasree from './Bhavyasreeclass';
+
+function App() {
+  return (
+    <div className="App">
+     <Bhavyasree/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACTJS/app15 class components-intro/src/Bhavyasreeclass.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+class Bhavyasree extends Component{
+    render(){
+        return <h1>bhavya</h1>
+    }
+}
+export default Bhavyasree
