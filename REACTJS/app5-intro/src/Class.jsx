606a8910fbb8f1d8a161dced778c1f34d44c9ff5
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-04 12:11:29 +0530

    react-js

--- REACTJS/app5-intro/src/Class.jsx
+++ REACTJS/app5-intro/src/Class.jsx
@@ -6,18 +6,22 @@ export default class Class extends Component{
         this.state={
             fname:"sree",
             lname:"sriramula",
-            contact:9392470961
+            contact:9392470961,
+            mothername:"lakshmi",
+            fathername:"ramesh"
         }
     }
     changename=()=>{
-        this.setState({fname:"ram",lname:"bhavyasree",contact:9676825370})
+        this.setState({fname:"ram",lname:"bhavyasree",contact:9676825370,mothername:"lakshmisree",fathername:"rameshbabu"})
     }
     render(){
         return<div>
             <button onClick={this.changename}>change name</button>
             <h2>{this.state.fname}</h2>
             <h2>{this.state.lname}</h2>
-            <h2>{this.state.contact}</h2> 
+            <h2>{this.state.contact}</h2>
+            <h2>{this.state.mothername}</h2> 
+            <h2>{this.state.fathername}</h2>
         </div>
     }
 }
