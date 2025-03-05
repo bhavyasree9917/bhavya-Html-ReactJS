343c7814a8bc4166afc7b0380fc756ef0e01ef07
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-05 20:48:32 +0530

    react-js

--- REACTJS/app9-intro/src/App.js
+++ REACTJS/app9-intro/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Viddu from './Viddu';
 
 function App() {
   return (
     <div className="App">
      <h2>reactjs projects</h2>
+     <Viddu/>
     </div>
   );
 }
