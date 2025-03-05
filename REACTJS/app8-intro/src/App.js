6b3d2ddac8568d3e5ffe6680deb289ba36891f87
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-05 20:47:26 +0530

    react-js

--- REACTJS/app8-intro/src/App.js
+++ REACTJS/app8-intro/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Prem from './Prem';
 
 function App() {
   return (
     <div className="App">
     <p>bhavyasree</p>
-  
+  <Prem/>
     </div>
   );
 }
