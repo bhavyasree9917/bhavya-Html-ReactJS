74c80f7d8d314a2299ef55349a06bd7a360e688c
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-06 20:25:52 +0530

    react-js

--- REACTJS/app11-intro/src/App.js
+++ REACTJS/app11-intro/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
 
 function App() {
   return (
     <div className="App">
-  <h2>reactjs projects</h2>
+  <User/>
     </div>
   ); 
 
