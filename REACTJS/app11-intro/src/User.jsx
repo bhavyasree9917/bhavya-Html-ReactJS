b528d32ee7769ab71cb55f7d5e92789f54c865ba
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-08 13:12:44 +0530

    react-js

--- REACTJS/app11-intro/src/User.jsx
+++ REACTJS/app11-intro/src/User.jsx
@@ -129,7 +129,7 @@ class User extends Component {
                 </tr>
               );
             })}
-            7          </tbody>
+                  </tbody>
         </table>
       </div>
     );
