2a2175189e942a25185289ef8c18eb318a1c05df
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-10 12:12:03 +0530

    react-js

--- REACTJS/app10-intro/src/App.js
+++ REACTJS/app10-intro/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Sony from './Sony';
+import User from './User';
+// import User from '../../app11-intro/src/User';
 function App() {
   return (
     <div className="App">
-    <Sony/>
+    {/* <Sony/> */}
+    <User/>
     </div>
   );
 }
--- REACTJS/app10-intro/src/User.jsx
+++ REACTJS/app10-intro/src/User.jsx
@@ -1,44 +1,41 @@
 import { Component } from "react";
 
 export default class User extends Component{
-    constructor(){
+   constructor(){
     super()
     this.state={
-      fname:"sree",
-      lname:"ram",
-      email:"sree ram @gmail.com"
-    }
-    Users=[
-
-    ]
-    editindex=null;
-
-    }
-    render(){
-        return(
-            <div>
-                <label htmlFor="">firstname:</label>
-                <input type="text" 
-                name="fname"
-                id=""
-                value={this.state.fname}
-                onChange={this.handlechange}/>{""}
-                <br/>
-                <label htmlFor="">last name:</label>
-                <input type="text"
-                id=""
-                name="lname"
-                value={this.state.lname} 
-                onChange={this.handlechange}/>{""}
-                <br/>
-                <label htmlFor="">email:</label>
-                <input type="text" 
-                id=""
-                name="email"
-                value={this.state.email}
-                onChange={this.handlechange}
-                />{""}
-            </div>
-        )
-    }
+        fnmae:"",
+        lnmae:"",
+        email:"",
+        users:[],
+        editindex:null,
+    
+}
+   };
+   render(){
+    return<div>
+        <form action="
+        ">
+            <label htmlFor="">firstname:</label>
+            <input type="text" 
+            nmae="fname"
+            value={this.state.fnmae}
+            onchange={this.handlechange}/>{""}
+            <br/>
+            <label htmlFor="
+            ">lastname:</label>
+            <input type="text" 
+            nmae="lastname"
+            value={this.state.lnmae}
+            onchange={this.handlechange}/>{""}
+            <br/>
+            <label htmlFor="">email:</label>
+            <input type="text"
+            name="email"
+            value={this.state.email}
+            onchange={this.handlechange} />{""}
+        </form>
+    </div>
+   }
+  
 }
