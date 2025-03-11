5cad5155ee0c8b08eea9d2c9fb505b55e0b82381
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-11 12:12:30 +0530

    react-js

--- REACTJS/app9-intro/src/App.js
+++ REACTJS/app9-intro/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Viddu from './Viddu';
+import User2 from './User2';
 
 function App() {
   return (
     <div className="App">
      <h2>reactjs projects</h2>
-     <Viddu/>
+     {/* <Viddu/> */}
+     <User2/>
      
     </div>
   );
--- REACTJS/app9-intro/src/User2.jsx
+++ REACTJS/app9-intro/src/User2.jsx
@@ -1,28 +1,123 @@
 import { Component } from "react";
 
-export default class User2 extends Component{
-    constructor(){
-        super()
-        this.state={
-           fname:"sree",
-           lname:"bhavyasree",
-           email:"bhavyasre9917@gmail.com"
-        },
-        User=[
-
-        ],
-        editindex=null;
-
+export default class User2 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        fname: "",
+        lname: "",
+        age: ""
+      },
+      users: [],
+      editindex: null,
     };
-    render(){
-        return<div>
-            <label htmlFor="">first name:</label>
-            <input
-             type="text"
-             nmae="fname"
-             id=""
-             value={this.state.fname}
-             onchange={this.handlechange} />
-        </div>
-    }
+  }
+
+  handleChange = (e) => {
+    const newPerson = { ...this.state.person };
+    newPerson[e.target.name] = e.target.value;
+    this.setState({ person: newPerson });
+  };
+
+  addUser = () => {
+    const newUsers = [...this.state.users];
+    newUsers.push(this.state.person);
+    this.setState({ users: newUsers });
+    this.clearUser();
+  };
+
+  clearUser = () => {
+    this.setState({ person: { fname: "", lname: "", age: "" } });
+  };
+
+  handleDelete = (i) => {
+    const newUsers = [...this.state.users];
+    newUsers.splice(i, 1);
+    this.setState({ users: newUsers });
+  };
+
+  handleEdit = (user, i) => {
+    this.setState({ person: user, editindex: i });
+  };
+
+  handleUpdate = () => {
+    const newUsers = [...this.state.users];
+    newUsers[this.state.editindex] = this.state.person;
+    this.setState({ users: newUsers, editindex: null });
+    this.clearUser();
+  };
+
+  render() {
+    return (
+      <div>
+        <form action="">
+          <label htmlFor="">First Name:</label>
+          <input
+            type="text"
+            name="fname"
+            value={this.state.person.fname}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label htmlFor="">Last Name:</label>
+          <input
+            type="text"
+            name="lname"
+            value={this.state.person.lname}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label htmlFor="">Age:</label>
+          <input
+            type="text"
+            name="age"
+            value={this.state.person.age}
+            onChange={this.handleChange}
+          />
+          <br />
+          {this.state.editindex === null ? (
+            <button type="button" onClick={this.addUser}>
+              Add User
+            </button>
+          ) : (
+            <button type="button" onClick={this.handleUpdate}>
+              Update User
+            </button>
+          )}
+        </form>
+        <hr />
+        <table border={2}>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Age</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map((user, i) => (
+              <tr key={i}>
+                <td>{user.fname}</td>
+                <td>{user.lname}</td>
+                <td>{user.age}</td>
+                <td>
+                  <button onClick={() => this.handleEdit(user, i)}>
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button onClick={() => this.handleDelete(i)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
