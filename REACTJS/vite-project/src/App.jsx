22e46bda1077ff062e6041b171284cea33a17995
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-12 15:26:21 +0530

    react-js

--- REACTJS/vite-project/src/App.jsx
+++ REACTJS/vite-project/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Users from './Users'
+import Ecommerse from './ecommerse'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Users/>
+    {/* <Users/> */}
+    <Ecommerse/>
     </>
   )
 }
