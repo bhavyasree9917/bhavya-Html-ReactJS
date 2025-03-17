000c08bc8ef6d8636a9c724fab8d73776c187779
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-17 18:46:16 +0530

    react-js

--- REACTJS/app4-intro/src/App.js
+++ REACTJS/app4-intro/src/App.js
@@ -7,7 +7,6 @@ function App() {
      <p>  Pawan Kalyan was born as Konidala Kalyan Kumar on 2 September 1971 in a Telugu family to
      Konidala Venkata Rao and Anjana Devi in Bapatla, Andhra ...</p>
     </div>
-
   );
 }
 
