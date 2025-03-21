7204a61d4896a3b76c5f38beac71e15990c10e77
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-21 20:08:29 +0530

    react-js

---
+++ REACTJS/render-props/src/Parent.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import Child1 from "./Child1";
+import Child2 from "./Child2";
+import Common from "./Common";
+
+export default class Parent extends Component {
+    render() {
+        return <div>
+            <Common render={(count, increment, decrement, reset) => (
+                <Child1 count={count}
+                    increment={increment}
+                    decrement={decrement}
+                    reset={reset} />
+
+
+            )} />
+            <Common render={(count, increment, decrement, reset) => (
+                <Child2 count={count}
+                    increment={increment}
+                    decrement={decrement}
+                    reset={reset} />
+
+            )} />
+
+
+        </div>
+
+    }
+}
