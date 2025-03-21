ada6afe7824b0e3236bc624b5a144a52418f5a27
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-21 20:08:13 +0530

    react-js

---
+++ REACTJS/render-props/src/Common.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+export default class Common extends Component{
+   constructor(){
+super()
+this.state={
+   count:100
+}}
+increment=()=>{
+   this.setState({ count:this.state.count+200})
+
+}
+decrement=()=>{
+   this.setState({count:this.state.count-200})
+
+}
+reset=()=>{
+   this.setState({count:0})
+
+}
+   
+   render(){
+      return<div>
+         {this.props.render(this.state.count,this.increment,this.decrement,this.reset)}
+      </div>
+
+   }
+   
+}
