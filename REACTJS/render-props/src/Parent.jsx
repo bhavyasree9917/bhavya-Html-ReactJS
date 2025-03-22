47e35d7c2f4916ff5ac5f6848a723a6568af56a9
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-22 12:38:15 +0530

    react-js

--- REACTJS/render-props/src/Parent.jsx
+++ REACTJS/render-props/src/Parent.jsx
@@ -19,7 +19,6 @@ export default class Parent extends Component {
                     increment={increment}
                     decrement={decrement}
                     reset={reset} />
-
             )} />
 
 
