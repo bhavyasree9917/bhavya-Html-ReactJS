c798817a73c78e64efd55d3df171a459b4335d48
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-22 12:35:11 +0530

    react-js

--- REACTJS/render-props/src/Child1.jsx
+++ REACTJS/render-props/src/Child1.jsx
@@ -8,5 +8,6 @@ export default class Child1 extends Component{
         <button onClick={this.props.decrement}>decrement</button>
         <button onClick={this.props.reset}>reset</button>
        </div> 
+       
     }
 }
