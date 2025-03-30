a43d07f25a32e992886ea33d7fc3aba4f65dcb8d
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-30 20:12:20 +0530

    contextapi

---
+++ REACTJS/contextAPI/src/context-components/Childcomponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import UserComponent from './Usercomponent'
+
+export default class ChildComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Child Component !</h2>
+        <hr />
+        <UserComponent message={this.props.message}/>
+      </div>
+    )
+  }
+}
