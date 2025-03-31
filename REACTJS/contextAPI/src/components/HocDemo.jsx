c1a892451adcb7f7b1f7434cb3daa559dc386eb3
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-03-31 19:48:35 +0530

    contextapi

--- REACTJS/contextAPI/src/components/HocDemo.jsx
+++ REACTJS/contextAPI/src/components/HocDemo.jsx
@@ -28,6 +28,7 @@ const HOCDemo = (ClickCounterDemo) => {
           handleIncrement={handleIncrement}
           handleDecrement={handleDecrement}
           handleReset={handleReset}
+          
         />
       );
     }
