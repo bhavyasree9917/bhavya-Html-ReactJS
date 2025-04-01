4208d5b328b86f4da7274c67e196152dbedfd690
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-01 16:10:51 +0530

    singlepage-applications

---
+++ REACTJS/single-page-applications/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import Contact from './CONTACT.JSX';
+import About from './About';
+import Projects from './Projects';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+<h2>Hello I am from Header</h2>
+      <Router>
+        <nav>
+        <Link to={''}>Home</Link>
+        <Link to={'about'}>About</Link>
+        <Link to={'contact'}>Contact</Link>
+        <Link to={'projects'}>Projects</Link>
+        </nav>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Router>
+
+      <h2>Hello I am from Footer</h2>
+      
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ REACTJS/single-page-applications/src/Projects.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Projects extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return<div>
+            <h1>welocome to project component</h1>
+        </div>
+    }
+}
