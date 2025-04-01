d50ad92bf632cce14424df766c8b0bc36baf3f75
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-01 16:10:23 +0530

    singlepage-applications

---
+++ REACTJS/single-page-applications/src/Home.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Home extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return<div>
+            <h1>welocome to home component</h1>
+        </div>
+    }
+}
