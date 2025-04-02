6c26d09c35eb187a3c6efd7fc9c3e7153cdbda62
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-02 12:48:50 +0530

    single-page-application

--- REACTJS/single-page-applications/src/Projects.jsx
+++ REACTJS/single-page-applications/src/Projects.jsx
@@ -6,7 +6,7 @@ export default class Projects extends Component{
     }
     render(){
         return<div>
-            <h1>welocome to project component</h1>
+            <h1>welocome to project component!!</h1>
         </div>
     }
 }
