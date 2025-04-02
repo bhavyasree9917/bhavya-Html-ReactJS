e039833a2c682c34210d6225f85e2588477ab3cb
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-02 12:46:29 +0530

    single-page-application

--- REACTJS/single-page-applications/src/Contact.jsx
+++ REACTJS/single-page-applications/src/Contact.jsx
@@ -6,7 +6,7 @@ export default class Contact extends Component{
     }
     render(){
         return<div>
-            <h1>welcome to contact component!!!!</h1>
+            <h1>welcome to contact component</h1>
         </div>
     }
 }
