83aa6779d7ce9b9f2d99638700fedf2aee8ba19d
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-03 15:07:49 +0530

    spa-projects

---
+++ REACTJS/spa-project/src/Users.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to User Details Page !!</h2>
+      </div>
+    )
+  }
+}
