d430ee7737eb0f823baaf845b3f763f5656cc8d8
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-07 16:16:02 +0530

    react-rnder-props

--- REACTJS/react-render-props/src/App.jsx
+++ REACTJS/react-render-props/src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Maincd from './Maincd'
+import Rendr from './Rendr'
+import Mouseinput from './Mouseinput'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Maincd/>
-
+{/* <Maincd/> */}
+{/* <Rendr/> */}
+<Mouseinput/>
     </>
   )
 }
