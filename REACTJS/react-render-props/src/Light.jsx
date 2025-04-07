059d6d17fd95c94b4d0333e43b70b1803c35e780
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-07 16:13:36 +0530

    react-render-props

---
+++ REACTJS/react-render-props/src/Light.jsx
@@ -0,0 +1,14 @@
+import React,{ Component } from "react";
+ 
+ export default class Light extends Component{
+   
+     render() {
+         return <div> 
+             <button onMouseOver={this.props.toggle}>{this.props.isOn ? 'Turn Off' : 'Turn On'}</button>
+             
+               The light is <strong>{this.props.isOn ? 'OFF' : 'ON'}</strong>
+             
+         </div>
+     }
+ }
+ 
