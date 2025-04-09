fb95aa7eb57723d814a2f60b4f5ce19f849141b5
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-09 19:36:27 +0530

    spa-project

--- REACTJS/spa-project/src/App.jsx
+++ REACTJS/spa-project/src/App.jsx
@@ -20,7 +20,6 @@ function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="userdetails" element={<Users />} />
         </Routes>
-        
       </Router>
     </>
   );
