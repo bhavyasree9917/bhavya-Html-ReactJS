7c8a730377a77c0371c4a7d0325c431767cb9377
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-11 11:01:18 +0530

    practice

--- REACTJS/practice/src/Child1.jsx
+++ REACTJS/practice/src/Child1.jsx
@@ -1,12 +1,12 @@
 import { Component } from "react";
 
 export default class Child1 extends Component{
-render(){
-    return<div>
-        <h1>count:{this.props.count}</h1>
-        <button onClick={this.props.increment}>increment</button>
-        <button onClick={this.props.decrement}>decrement</button>
-        <button onClick={this.props.reset}>reset</button>
-    </div>
-}
+    render(){
+        return<div>
+            <h2>count:{this.props.count}</h2>
+            <button onClick={this.props.increment}>increment</button>
+            <button onClick={this.props.decrement}>decrement</button>
+            <button onClick={this.props.reset}>reset</button>
+        </div>
+    }
 }
