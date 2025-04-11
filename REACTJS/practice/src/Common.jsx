9c1b7b1f567139c6a055760020524917bc91f5d1
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-11 11:01:33 +0530

    practice

--- REACTJS/practice/src/Common.jsx
+++ REACTJS/practice/src/Common.jsx
@@ -1,24 +1,27 @@
 import { Component } from "react";
 
-export default class Common extends Component{
-    constructor(){
+export default class Common extends Component {
+    constructor() {
         super()
-        this.state={
-            count:10
+        this.state = {
+            count: 10
         }
-        
     }
-    increment=()=>{
-        this.setState=({count:this.state.count+10})
+    increment = () => {
+        this.setState({ count: this.state.count + 10 })
+
     }
-    decrement=()=>{
-        this.setstate=({count:this.state.count-10})
+    decrement = () => {
+        this.setState({ count: this.state.count - 10 })
+
     }
-    reset=()=>{
-        this.setstate=({count:0})
+    reset = () => {
+        this.setState({ count: 0 })
+
     }
-    render(){
-        return<div> {this.props.render(this.state.count, this.increment, this.decrement, this.reset)}
+    render() {
+        return <div>
+            {this.props.render(this.state.count, this.increment, this.decrement, this.reset)}
         </div>
     }
 }
