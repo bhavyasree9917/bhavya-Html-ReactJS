739abdc068429b3297180b6b5151a4265e3670c9
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-14 19:02:51 +0530

    contextapi

--- REACTJS/contextAPI/src/context-components/ContextDemo.jsx
+++ REACTJS/contextAPI/src/context-components/ContextDemo.jsx
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 
 export const UserContext = createContext();
+
 export const UserContextProvider = UserContext.Provider; // Use this when you add the data 
 export const UserContextConsumber = UserContext.Consumer; // Use this when you receive the data;
