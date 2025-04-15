0b60e16a67d496410c736c3cbb00708048452837
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-15 15:05:06 +0530

    react-contextapi

--- REACTJS/react-contextapi/src/Chi2.jsx
+++ REACTJS/react-contextapi/src/Chi2.jsx
@@ -6,11 +6,5 @@ export default class Chi2 extends Component{
     render(){
         return (<div><h2>iam from Chi2</h2>
              <h1> <Chi3 msg1={this.props.msg1}/></h1></div>) 
-            
-
-        
-        
-
-
     }
 }
