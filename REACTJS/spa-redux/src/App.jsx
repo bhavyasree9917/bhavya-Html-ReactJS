6de9214718bdd7850f3d84b6bd4e2b7d5c3b9673
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-16 12:48:30 +0530

    redux

--- REACTJS/spa-redux/src/App.jsx
+++ REACTJS/spa-redux/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Users from "./Components/Users";
+import Employees from "./Components/Employees";
+import Students from "./Components/Students";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    
+      <Users />
+      <hr />
+      <Employees />
+      <hr />
+      <Students />
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
