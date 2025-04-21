78cb83f02a5c2628ebbf6ec03af98a3ad2e124ab
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-21 10:47:22 +0530

    FUNCTION-RENDERPROPOS

--- REACTJS/functional-render-props/src/App.jsx
+++ REACTJS/functional-render-props/src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import FuncClickCounter from "./components/FuncClickCounter";
+import FuncHoverCouter from "./components/FuncHoverCouter";
+import FuncRenderPropsDemo from "./components/FuncRenderFuncRenderPropsDemo";
+
+const NewComponent = () => {
+  return <h2>New Component Message</h2>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-  
+    <>   
+      
+      <FuncRenderPropsDemo
+        render={(count, handleIncrement, handleDecrement, handleReset) => (
+          <FuncHoverCouter
+            count={count}
+            handleIncrement={handleIncrement}
+            handleDecrement={handleDecrement}
+            handleReset={handleReset}
+          />
+        )}
+      />
+      <FuncRenderPropsDemo
+        render={(count, handleIncrement, handleDecrement, handleReset) => (
+          <FuncClickCounter
+            count={count}
+            handleIncrement={handleIncrement}
+            handleDecrement={handleDecrement}
+            handleReset={handleReset}
+          />
+        )}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
