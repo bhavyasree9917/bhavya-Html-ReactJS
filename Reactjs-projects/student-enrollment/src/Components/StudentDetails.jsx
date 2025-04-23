32a7349f0e9f5210c9290c3b9ec3354765c5c506
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-23 19:24:40 +0530

    react-projects

--- Reactjs-projects/student-enrollment/src/Components/StudentDetails.jsx
+++ Reactjs-projects/student-enrollment/src/Components/StudentDetails.jsx
@@ -84,9 +84,7 @@ const StudentDetails = () => {
                 <button onClick={() => handleDelete(i)}>Delete Student</button>
               </td>
             </tr>
-            
           ))}
-
         </tbody>
       </table>
     </div>
