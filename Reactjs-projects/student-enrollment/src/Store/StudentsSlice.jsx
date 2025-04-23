82188442a41a7f6f1bca04fca82156a03089b645
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-23 19:33:03 +0530

    react-projects

--- Reactjs-projects/student-enrollment/src/Store/StudentsSlice.jsx
+++ Reactjs-projects/student-enrollment/src/Store/StudentsSlice.jsx
@@ -2,11 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   students: [
-    { id: 1, name: "Yamuna", age: 21, email: "Yamuna@gmail.com" },
-    { id: 2, name: "Bhaya", age: 21, email: "Bhaya@gmail.com" },
-    { id: 3, name: "Savithri", age: 30, email: "Savithri@gmail.com" },
-    { id: 4, name: "Neeraja", age: 17, email: "Neeraja@gmail.com" },
-    { id: 5, name: "Chenchaiah", age: 30, email: "Chenchaiah@gmail.com" }
+    { id: 1, name: "pawan", age: 21, email: "pawan@gmail.com" },
+    { id: 2, name: "ram", age: 21, email: "ram@gmail.com" },
+    { id: 3, name: "sree", age: 30, email: "Sree@gmail.com" },
+    { id: 4, name: "bhavya", age: 17, email: "bhavya@gmail.com" },
+    { id: 5, name: "kalyan", age: 30, email: "kalyan@gmail.com" }
   ]
 };
 
