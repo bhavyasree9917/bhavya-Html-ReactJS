ffb8c3e6d799965413f3323dd16ce1ca2322b485
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-23 19:35:55 +0530

    react-projects

--- Reactjs-projects/student-enrollment/src/Store/StudentsSlice.jsx
+++ Reactjs-projects/student-enrollment/src/Store/StudentsSlice.jsx
@@ -4,7 +4,7 @@ const initialState = {
   students: [
     { id: 1, name: "pawan", age: 34, email: "pawan@gmail.com" },
     { id: 2, name: "ram", age: 28, email: "ram@gmail.com" },
-    { id: 3, name: "sree", age: 18, email: "Sree@gmail.com" },
+    { id: 3, name: "sree", age: 18, email: "Sreekonidela@gmail.com" },
     { id: 4, name: "bhavya", age: 22, email: "bhavyasree@gmail.com" },
     { id: 5, name: "kalyan", age: 52, email: "kalyan@gmail.com" }
   ]
