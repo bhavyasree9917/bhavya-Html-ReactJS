9803d3f2f8fd6703892f889894a9e355ade4cfc9
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-25 19:51:49 +0530

    movie-details

---
+++ Reactjs-projects/movie-database/src/Store/Store.jsx
@@ -0,0 +1,11 @@
+// Store/Store.js
+import { configureStore } from "@reduxjs/toolkit";
+import MoviesSlice from "./MoviesSlice"; // Import the MoviesSlice reducer
+
+const store = configureStore({
+  reducer: {
+    movies: MoviesSlice, // Add the MoviesSlice reducer to the store
+  },
+});
+
+export default store; // Export the store as default
