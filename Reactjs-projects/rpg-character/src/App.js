3868f009bee1dadcc099a0fb8749696ed38a0d88
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-27 18:48:39 +0530

    movie-database

---
+++ Reactjs-projects/rpg-character/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import CharacterComponent from './CharacterComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <CharacterComponent />
+    </div>
+  );
+}
+
+export default App;
