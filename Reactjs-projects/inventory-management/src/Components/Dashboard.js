c72b0fb2ff3df5191fb608f422fad80daf067af7
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-29 20:56:32 +0530

    inventory

--- Reactjs-projects/inventory-management/src/Components/Dashboard.js
+++ Reactjs-projects/inventory-management/src/Components/Dashboard.js
@@ -7,7 +7,7 @@ function Dashboard({ inventory }) {
       <h2>Dashboard</h2>
       <p>Total Inventory Value: {inventory.totalInventoryValue}</p>
       <p>Low Stock Products: {inventory.lowStockProducts.length}</p>
-      <p>Categories: {inventory.categories.length}</p>
+      <p>Categories {inventory.categories.length}</p>
       <p>Most Sold Products {inventory.mostSoldProducts.map(p => p.name).join(', ')}</p>
     </div>
     
