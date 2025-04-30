c29e6797ae7ce92d626c17d1b349f6cebc155467
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-04-30 20:06:37 +0530

    music

--- Reactjs-projects/music-player/src/store/musicPlayerSlice.jsx
+++ Reactjs-projects/music-player/src/store/musicPlayerSlice.jsx
@@ -4,8 +4,6 @@ import { createSlice } from '@reduxjs/toolkit';
 const exampleSongs = [
  
     { id: 1, title: 'Song 1', artist: 'Artist 1', duration: 180, url: '/songs/song1.mp3' },
-
-  
   ,
   { id: 2, title: 'Song 2', artist: 'Artist 2', duration: 240 },
   { id: 3, title: 'Song 3', artist: 'Artist 3', duration: 200 },
