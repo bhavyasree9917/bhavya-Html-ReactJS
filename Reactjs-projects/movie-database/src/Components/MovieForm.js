931d6a8eb782462695ed0538e242ebb5f157e773
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-01 20:32:36 +0530

    movie

--- Reactjs-projects/movie-database/src/Components/MovieForm.js
+++ Reactjs-projects/movie-database/src/Components/MovieForm.js
@@ -38,7 +38,7 @@ const MovieForm = ({ addMovie }) => {
       />
       <input
         type="text"
-        placeholder=""
+        placeholder="Director"
         value={director}
         onChange={(e) => setDirector(e.target.value)}
         required
