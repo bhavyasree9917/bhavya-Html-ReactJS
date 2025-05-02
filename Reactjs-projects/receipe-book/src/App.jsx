30d392a400d88d08a2645cc3d0e3aba71b8c40ff
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-02 20:56:00 +0530

    receipe

--- Reactjs-projects/receipe-book/src/App.jsx
+++ Reactjs-projects/receipe-book/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const appId = 'your_actual_app_id'; 
-  const appKey = 'your_actual_app_key'; 
+  const appKey = 'your_actual_app_key';
+   
 
   
   const submitHandler = async (e) => {
