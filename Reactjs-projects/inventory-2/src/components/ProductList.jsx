974d463876ceee637acfd737b391f4bffdc38e75
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-05 19:09:42 +0530

    inventory2

--- Reactjs-projects/inventory-2/src/components/ProductList.jsx
+++ Reactjs-projects/inventory-2/src/components/ProductList.jsx
@@ -34,7 +34,7 @@ const ProductList = ({ onEdit }) => {
           <p><strong>{product.name}</strong> - {product.quantity} in stock</p>
           <p>Price: ${product.price} | Total Price: ${calculateTotalPrice(product)}</p>
           <button onClick={() => onEdit(product)}>✏️ Edit</button>
-          <button onClick={() => dispatch(removeProduct(product.name))}>🗑️ Remove</button>
+          <button onClick={() => dispatch(removeProduct(product.name))}>🗑️ </button>
           <div>
             <button onClick={() => dispatch(updateQuantity({ productName: product.name, quantity: 1 }))}>Increase</button>
             <button onClick={() => dispatch(updateQuantity({ productName: product.name, quantity: -1 }))}>Decrease</button>
