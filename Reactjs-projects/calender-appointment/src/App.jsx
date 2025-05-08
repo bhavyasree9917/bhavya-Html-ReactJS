f06905f382d5b26bd52840c0726bd2dedfed8cf1
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-08 11:42:11 +0530

    calender

--- Reactjs-projects/calender-appointment/src/App.jsx
+++ Reactjs-projects/calender-appointment/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import AppointmentForm from './components/AppointmentForm';
+import AppointmentList from './components/AppointmentList ';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [editData, setEditData] = useState(null);
 
   return (
-    <>
-    </>
-  )
-}
+    <Provider store={store}>
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">📅 Calendar Appointments</h1>
+        <AppointmentForm editData={editData} setEditData={setEditData} />
+        <AppointmentList setEditData={setEditData} />
+      </div>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
