dc433b484a0c6a3104bb90807240210277300093
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-13 19:45:31 +0530

    inventory

--- Reactjs-projects/inventory-2/src/components/ProductForm.jsx
+++ Reactjs-projects/inventory-2/src/components/ProductForm.jsx
@@ -73,7 +73,7 @@ const ProductForm = ({ selectedProduct, clearSelection }) => {
         value={price}
         onChange={(e) => setPrice(Number(e.target.value))}
       />
-      <label htmlFor="Supplier">Supplier:</label>
+      <label htmlFor="Supplier"></label>
       <input
         type="text"
         placeholder="Supplier"
