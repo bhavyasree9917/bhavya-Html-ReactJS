c1ac2cebd4b880ba13ef19395583f33d0354836e
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-15 18:37:32 +0530

    receipe-generator

---
+++ Reactjs-projects/Receipe-generator/src/components/IngredientForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addIngredient } from '../redux/recipeSlice';
+
+const IngredientForm = () => {
+  const dispatch = useDispatch();
+  const [ingredient, setIngredient] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addIngredient({ name: ingredient }));
+    setIngredient('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="p-4 border rounded mb-4">
+      <input 
+        placeholder="Add Ingredient" 
+        value={ingredient} 
+        onChange={(e) => setIngredient(e.target.value)} 
+        required 
+        className="block w-full mb-2 border p-2" 
+      />
+      <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded">
+        Add Ingredient
+      </button>
+    </form>
+  );
+};
+
+export default IngredientForm;
