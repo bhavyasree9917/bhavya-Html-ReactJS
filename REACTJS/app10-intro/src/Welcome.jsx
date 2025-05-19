340eba80cd0da35335e7fe6ea8156bf86a2d4764
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-19 19:32:42 +0530

    reactpractice

---
+++ REACTJS/app10-intro/src/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Welcome extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return<div>
+            <h1>welocome to the componnet</h1>
+        </div>
+    }
+}
