f74e2dab3fecbfa87e2f1b78750808d2c7613b5e
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-19 19:32:54 +0530

    reactpractice

---
+++ REACTJS/app12-intro/src/Lifecycle.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class Lifecycle extends Component{
+    constructor(){
+        super()
+        this.state={
+            count:0
+        }
+    }
+    timer=()=>{
+        this.setState({})
+    }
+
+    render(){
+return<div>
+   <button onClick={this.timer}>start timer</button> 
+</div>
+    }
+
+}
+export default Lifecycle
