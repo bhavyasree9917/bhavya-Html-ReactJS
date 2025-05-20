e01b43b379d67d0ac5be13805f3f600dbb6603fd
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-20 14:56:54 +0530

    react-js

---
+++ REACTJS/reactpractice-communication/src/Title.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import TitleDisplay from "./TitleDisplay";
+
+export default class Title extends Component{
+    constructor(){
+        super()
+       
+    }
+    render(){
+        const title="bhavya"
+        return<div>
+<TitleDisplay ti={title}/>
+        </div>
+    }
+}
