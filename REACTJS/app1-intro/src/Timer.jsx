9c4da0da3d9e7963540ae4cc0e6bcfad7351e6de
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-20 14:55:05 +0530

    react-js

---
+++ REACTJS/app1-intro/src/Timer.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+export default class Timer extends Component{
+    constructor(){
+        super()
+        this.state={
+            seconds:0,
+            intervalId:null
+        }
+    }
+    componentDidMount=()=>{
+        this.state.intervalId=setInterval(()=>{
+            this.setState({
+                seconds:this.state.seconds+1
+            })
+        },1000)
+    }
+    componentWillUnmount=()=>{
+        clearInterval(this.state.intervalId)
+    }
+    render(){
+        return<div>
+            <h1>timer:{this.state.seconds}</h1>
+        </div>
+    }
+}
