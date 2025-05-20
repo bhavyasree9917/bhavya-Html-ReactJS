eee1b328ad68332699ecd8a8c6282c1ad53de2ae
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-20 14:54:41 +0530

    react-js

---
+++ REACTJS/app1-intro/src/Greeting.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import Greetingprop from "./Greetingprop";
+
+export default class Greeting extends Component{
+    constructor(){
+        super()
+        this.state={
+            person:{
+                name:"sriramula bhavyasree"
+            }
+        }
+    }
+    render(){
+        return<div>
+            <h1>{this.state.person.name}</h1>
+            <Greetingprop all={this.state.person.name}/>
+        </div>
+    }
+}
