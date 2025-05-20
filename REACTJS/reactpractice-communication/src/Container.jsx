af87741e449596d0362fb76341c585bddda7d8da
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-20 14:56:06 +0530

    react-js

--- REACTJS/reactpractice-communication/src/Container.jsx
+++ REACTJS/reactpractice-communication/src/Container.jsx
@@ -1,12 +1,25 @@
-import { Component } from "react";
+import { useState } from "react";
+import Inputchild from "./InputChild";
+import DisplayChild from "./DisplayChild";
 
-export default class Container extends Component{
-    constructor(){
-        super()
-    }
-    render(){
-        return<div>
-            
+const Container = () => {
+    const [inputtext, settext] = useState("");
+
+    const display = () => {
+        settext("bh");
+    };
+
+    const handleChange = (e) => {
+        settext(e.target.value);
+    };
+
+    return (
+        <div>
+            <button onClick={display}>Click Me</button>
+            <Inputchild inputtext={inputtext} />
+            <DisplayChild handleChange={handleChange} />
         </div>
-    }
-}
+    );
+};
+
+export default Container;
