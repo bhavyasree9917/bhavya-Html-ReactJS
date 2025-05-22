2c4612098e0bec428464323408db068a101199ce
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-22 19:59:20 +0530

    react-practice

---
+++ REACTJS/app3-intro/src/Counter.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+export default class Counter extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+    increment = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+    render() {
+        return <div>
+            <h1>{this.state.count}</h1>
+            <button onClick={this.increment}>increase the value</button>
+        </div>
+    }
+}
