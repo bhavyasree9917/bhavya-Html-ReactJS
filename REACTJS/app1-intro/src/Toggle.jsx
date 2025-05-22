7361eec186e57a1e092c639d783fd3f62031810e
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-22 19:59:35 +0530

    react-practice

--- REACTJS/app1-intro/src/Toggle.jsx
+++ REACTJS/app1-intro/src/Toggle.jsx
@@ -1,21 +1,21 @@
 import { Component } from "react";
 
-export default class Toggle extends Component{
-    constructor(){
+export default class Toggle extends Component {
+    constructor() {
         super()
-        this.state={
-            ison:true
+        this.state = {
+            ison: true
         }
     }
-    handlechange=()=>{
+    handletoggle = () => {
         this.setState({
-            ison:!this.state.ison
+            ison: !this.state.ison
         })
     }
-    render(){
-        return<div>
-            <h1>{this.state.ison?"on":"off"}</h1>
-            <button onClick={this.handlechange}>toggle change</button>
+    render() {
+        return <div>
+            <h1>{this.state.ison ? "on" : "off"}</h1>
+            <button onClick={this.handletoggle}>togglechange</button>
         </div>
     }
 }
