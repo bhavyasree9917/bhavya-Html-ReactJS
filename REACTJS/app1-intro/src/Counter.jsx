23cacf1df98789c736d9f1494e03fae8eaba11dc
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-22 19:57:09 +0530

    react-practice

--- REACTJS/app1-intro/src/Counter.jsx
+++ REACTJS/app1-intro/src/Counter.jsx
@@ -1,17 +1,17 @@
 import { Component } from "react";
 
-export default class Counter extends Component {
-    constructor() {
+export default class Counter extends Component{
+    constructor(){
         super()
-        this.state = {
-            count: 0
+        this.state={
+            count:0
         }
     }
-    increment = () => {
-        this.setState({ count: this.state.count + 1 })
+    increment=()=>{
+        this.setState({count:this.state.count+1})
     }
-    render() {
-        return <div>
+    render(){
+        return<div>
             <h1>count:{this.state.count}</h1>
             <button onClick={this.increment}>increment</button>
         </div>
