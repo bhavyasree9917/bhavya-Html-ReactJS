c84aa1c062743dc303e036b2c58f4608210bd793
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-23 12:55:13 +0530

    react-practice

--- REACTJS/reactpractice-functional/src/ColorChanger.jsx
+++ REACTJS/reactpractice-functional/src/ColorChanger.jsx
@@ -3,20 +3,9 @@ import React, { useState } from 'react';
 const ColorChanger = () => {
 
   const [bgColor, setBgColor] = useState('#ffffff');
-
-
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
   
   const handleChangeColor = () => {
-    setBgColor(getRandomColor());
+    setBgColor("red");
   };
 
   return (
