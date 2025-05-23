195b74a95f7795d897768c6e0da068ea06b48057
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-23 12:53:28 +0530

    react-practice

--- REACTJS/app5-intro/src/App.js
+++ REACTJS/app5-intro/src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Class from './Class';
 import Header from './Header';
+import Profile from './Profile';
+import ClickCounter from './ClickCounter';
+import Form from './Form';
+
 
 function App() {
   return (
     <div className="App">
-  <Header/>
- 
+  {/* <Header/> */}
+  {/* <Profile/> */}
+{/* 
+ <ClickCounter/> */}
+<Form/>
     </div>
   );
 }
