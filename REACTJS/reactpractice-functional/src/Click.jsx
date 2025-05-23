e0946ed94986aadffbe11447461778b58700dbde
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-23 12:54:52 +0530

    react-practice

---
+++ REACTJS/reactpractice-functional/src/Click.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+const Click = () => {
+    const [count, setcount] = useState(0);
+    const handleclick = () => {
+        setcount(count => count + 1)
+
+    }
+
+
+    return <div>
+        <h1>{count}</h1>
+        <button onClick={handleclick}>increase the count</button>
+
+    </div>
+}
+export default Click;
