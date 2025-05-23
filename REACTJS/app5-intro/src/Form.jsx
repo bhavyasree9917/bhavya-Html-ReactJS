0b6c5813d2fd845e700a73b77a1e52d4f8bab01b
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-23 12:54:04 +0530

    react-practice

---
+++ REACTJS/app5-intro/src/Form.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+
+const Form = () => {
+    const [name, setname] = useState("");
+    const [email, setemail] = useState("");
+
+    const handlenamechange = (e) => setname(e.target.value);
+    const handleemailchange = (e) => setemail(e.target.value)
+
+
+    return <div>
+        <h1>registaration form</h1>
+        <form action="">
+        <label>
+            fname
+        </label>
+        <input type="name"
+            value={name}
+            onChange={handlenamechange} />
+        <label >email</label>
+        <input type="email"
+            value={email}
+            onChange={handleemailchange} />
+            </form>
+            <p>name:{name}</p>
+            <p>email:{email}</p>
+
+    </div>
+}
+export default Form
