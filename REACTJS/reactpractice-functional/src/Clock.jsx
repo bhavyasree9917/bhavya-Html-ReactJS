c571996b034da1cd157df364d11ee041c66e54e2
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-24 19:07:19 +0530

    react-practice

--- REACTJS/reactpractice-functional/src/Clock.jsx
+++ REACTJS/reactpractice-functional/src/Clock.jsx
@@ -4,16 +4,16 @@ const Clock = () => {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
 
   useEffect(() => {
-   
+
     const timer = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
     }, 1000);
 
-    
+
     return () => {
       clearInterval(timer);
     };
-  }, []); 
+  }, []);
 
   return (
     <div>
