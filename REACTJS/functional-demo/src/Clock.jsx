35391324f18b94add80ddd8badbdffea7bcdf98d
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-25 12:30:57 +0530

    react

--- REACTJS/functional-demo/src/Clock.jsx
+++ REACTJS/functional-demo/src/Clock.jsx
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useEffect, useState } from "react";
 const Clock = () => {
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [time, setTime] = useState(new Date().toLocaleString());
+
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            setTime(new Date().toLocaleTimeString());
-        }, 1000);
+        const timer = setTime(() => {
+            setTime(new Date().toLocaleString())
+        }, 1000)
+        return () => {
+            clearInterval(timer)
+        }
+    })
 
-        return () => clearInterval(timer);
-    }, []);
+    return <div>
+        <h1>timer:{time}</h1>
 
-    return (
-        <div>
-            <h1>Timer</h1>
-            <p>{time}</p>
-        </div>
-    );
-};
-
-export default Clock;
+    </div>
+}
+export default Clock
