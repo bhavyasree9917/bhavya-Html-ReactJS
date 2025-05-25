a2f9f9ab32963781b27e3184e043550887f5e316
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-25 12:29:51 +0530

    react

--- REACTJS/functional-demo/src/Clickcounter.jsx
+++ REACTJS/functional-demo/src/Clickcounter.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 const Clickcounter = () => {
-    const [count, setcount] = useState(0);
+    const [count, setCount] = useState(0);
 
-    const handleclick = () => {
-        setcount(count => count + 1)
+    const handlecount = () => {
+        setCount(count => count + 1)
     }
 
 
     return <div>
         <h1>count:{count}</h1>
-        <button onClick={handleclick}>click the button</button>
-
+        <button onClick={handlecount}>increase</button>
     </div>
 }
 export default Clickcounter
