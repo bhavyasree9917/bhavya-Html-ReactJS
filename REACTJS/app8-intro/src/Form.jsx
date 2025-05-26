93a9e0034122167f1e6e8c7fc8aae74baef8f0e8
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-26 16:16:15 +0530

    practice

---
+++ REACTJS/app8-intro/src/Form.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+const Form = () => {
+    const [name, setname] = useState("");
+    const [email, setemail] = useState("");
+
+    const handlenamechange = (e) => setname(e.target.value);
+    const handleemailchange = (e) => setemail(e.target.value);
+
+    return <div>
+        <h1>user form details</h1>
+        <label>name</label>
+        <input type="text"
+            value={name}
+            onChange={handlenamechange} />
+        <label>email</label>
+        <input type="email"
+            value={email}
+            onChange={handleemailchange} />
+        <p>name:{name}</p>
+        <p>email:{email}</p>
+
+    </div>
+}
+export default Form
