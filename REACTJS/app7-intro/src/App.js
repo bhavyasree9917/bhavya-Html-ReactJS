e82e240707150584ced424844e75d6054ed45577
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-26 16:15:02 +0530

    practice

--- REACTJS/app7-intro/src/App.js
+++ REACTJS/app7-intro/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Kalyan from './Kalyan';
+import Header from './Header';
+import Profile from './Profile';
+import Clickcounter from './Clickcounter';
+import Form from './Form';
+import Clock from './Clock';
+import Avatar from './Avatar';
 
 function App() {
   return (
     <div className="App">
-    <h5>walcome to reactjs</h5>
-    <Kalyan/>
+   {/* <Header/> */}
+   {/* <Profile username="ram"/> */}
+   {/* <Clickcounter/> */}
+   {/* <Form/> */}
+   {/* <Clock/> */}
+   <Avatar imageurl={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNmFl3KGcuSzPGUUujb47OaWFN1vhYTcYPVg&s"}/>
+
     </div>
   );
 }
