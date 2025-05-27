0b6649c1b3a3705be7157bc614e141765adc5da8
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-27 10:01:39 +0530

    practice

---
+++ REACTJS/app2-intro/src/Clickcounter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+const Clickcounter = () => {
+    const [count, setcount] = useState(0);
+    const handleclick = () => {
+        setcount(count => count + 1)
+    }
+
+    return <div>
+        <h1>count:{count}</h1>
+        <button onClick={handleclick}>clickhere</button>
+
+    </div>
+}
+export default Clickcounter;
