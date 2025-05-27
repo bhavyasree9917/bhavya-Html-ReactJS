584b05b5f4e1055669fccb23e1fe0e2a521d4f68
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-27 10:01:25 +0530

    practice

--- REACTJS/app2-intro/src/App.js
+++ REACTJS/app2-intro/src/App.js
@@ -2,11 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Pal from './Pal';
 import TodoApp from './TodoApp';
+import Header from './Header';
+import Profile from './Profile';
+import Clickcounter from './Clickcounter';
+import Form from './Form';
+import Clock from './Clock';
+import Showhide from './Showhide';
+import Avatar from './Avatar';
 
 function App() {
   return (
-   <TodoApp/>
-    
+  //  <TodoApp/>
+  // <Header/>
+  // <Profile username="bhavya"/>
+  // <Clickcounter/>
+  // <Form/>
+  // <Clock/>
+  // <Showhide/>
+  
 
   );
   
