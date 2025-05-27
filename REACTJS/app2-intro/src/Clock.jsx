bf55c12c648e68c5777e2e2b834dd1cf5971cd2c
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-27 10:01:48 +0530

    practice

---
+++ REACTJS/app2-intro/src/Clock.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+const Clock = () => {
+    const [time, settime] = useState(new Date().toLocaleTimeString());
+    useEffect(() => {
+        const timer = setInterval(() => {
+            settime(new Date().toLocaleTimeString())
+        }, 1000)
+        return () => {
+            clearInterval(timer)
+        }
+    }, [])
+
+    return <div>
+        <h1>time:{time}</h1>
+
+    </div>
+}
+export default Clock
