de6bd350f1b06d0b1132c7086c3229e74e031064
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-28 15:50:20 +0530

    react-topics

---
+++ REACTJS/all-react-topics-50/src/Profile.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <p>Name: {this.props.name}</p>
+        <p>Age: {this.props.age}</p>
+      </div>
+    );
+  }
+}
+export default Profile
+
+
+
