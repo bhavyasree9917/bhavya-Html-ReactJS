d8a8db35d8e73efd6a2ecd8047d7d4675bf770b9
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-28 15:49:59 +0530

    react-topics

---
+++ REACTJS/all-react-topics-50/src/Message.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Message extends Component {
+  render() {
+    return <p>You have {this.props.count} new messages.</p>;
+  }
+}
+export default Message
+
+// Usage
+
