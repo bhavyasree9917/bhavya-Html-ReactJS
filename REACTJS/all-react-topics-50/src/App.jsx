a52cf3541c552fe155f5f1446f48853e839bd98a
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-29 19:48:54 +0530

    practice

--- REACTJS/all-react-topics-50/src/App.jsx
+++ REACTJS/all-react-topics-50/src/App.jsx
@@ -7,18 +7,45 @@ import Greeting from './Greeting'
 import Profile from './Profile'
 import Default from './default'
 import Message from './Message'
+import FocusInput from './FocusInput'
+import PreviousValue from './PreviousValue'
+import TodoApp from './TodoApp'
+import MemoApp from './MemoApp'
+import CallbackApp from './CallbackApp'
+import FetchData from './FetchData'
+import FetchStatus from './FetchStatus'
+import PollData from './PollData'
+import CancelFetch from './CancelFetch'
+import FetchPost from './Fetchpost'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Welcome/>
+      {/* <Welcome/> */}
       
-<Greeting name="John" />
-<Profile name="Alice" age={28} />
-<Default/>
-<Message count={3} />
+{/* <Greeting name="John" /> */}
+{/* <Profile name="Alice" age={28} /> */}
+{/* <Default/> */}
+{/* <Message count={3} /> */}
+
+
+
+
+
+
+
+{/* <FocusInput/> */}
+{/* <PreviousValue/> */}
+{/* <TodoApp/> */}
+{/* <MemoApp/> */}
+{/* <CallbackApp/> */}
+{/* <FetchData/> */}
+{/* <FetchStatus/> */}
+{/* <PollData/> */}
+{/* <CancelFetch/> */}
+<FetchPost/>
     </>
   )
 }
