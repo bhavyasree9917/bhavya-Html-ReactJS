3a310adbb6f9246337ca980aee69bd7b312ece90
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-29 19:49:26 +0530

    practice

---
+++ REACTJS/all-react-topics-50/src/FetchData.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+export default function FetchData() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  if (!data) return <div>Loading...</div>;
+
+  return <div>{data.title}</div>;
+}
