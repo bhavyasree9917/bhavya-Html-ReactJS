d168755d0aa2b6c30e4e152735ac5cb5dbd4abf4
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-05-29 19:49:19 +0530

    practice

---
+++ REACTJS/all-react-topics-50/src/CancelFetch.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+export default function CancelFetch() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1', { signal: controller.signal })
+      .then(res => res.json())
+      .then(setData)
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          console.log('Fetch canceled');
+        } else {
+          console.error(err);
+        }
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  if (!data) return <div>Loading...</div>;
+
+  return <div>{data.title}</div>;
+}
