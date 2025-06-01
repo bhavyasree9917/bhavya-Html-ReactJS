f17c442e7a0da497344cd40ef600c6f55d5919fb
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-01 15:06:52 +0530

    practice

---
+++ REACTJS/react-revision/src/Default.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Default extends  Component {
+  render() {
+    return <h1>Welcome, {this.props.name}!</h1>;
+  }
+}
+
+Default.defaultProps = {
+  name: 'Guest'
+};
+export default Default
