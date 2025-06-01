661d99cff87c8cb19f6155a66b162dc17a0a054a
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-01 15:07:53 +0530

    practice

---
+++ REACTJS/react-revision/src/Welcome.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+export default class Welcome extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return<div>
+            <h1>welcome</h1>
+        </div>
+    }
+}
