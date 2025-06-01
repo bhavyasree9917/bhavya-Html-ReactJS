8cb34838269cab6281ec468be3a0185e86808c38
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-01 15:07:27 +0530

    practice

---
+++ REACTJS/react-revision/src/Message.jsx
@@ -0,0 +1,13 @@
+
+
+import { Component } from "react";
+
+class Message extends Component {
+    render() {
+        return <p>You have {this.props.count} new messages.</p>;
+    }
+}
+export default Message
+
+// Usage
+
