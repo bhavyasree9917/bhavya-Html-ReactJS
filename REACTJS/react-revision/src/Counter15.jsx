1126bba390db3507531a9f260d4b2f89d31b51b0
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-02 19:31:46 +0530

    practice

---
+++ REACTJS/react-revision/src/Counter15.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Counter15() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increase</button>
+    </div>
+  );
+}
+export default Counter15;
