26290bb5df03b37745c001185744f11b6211dd9b
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-02 19:25:16 +0530

    practice

---
+++ REACTJS/react-revision/src/Counter7.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Counter7 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Counter App</h2>
+                <p>Count: {this.state.count}</p>
+                <button onClick={this.handleIncrement}>Increment</button>
+            </div>
+        );
+    }
+}
+
+export default Counter7;
