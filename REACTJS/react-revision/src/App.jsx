02c7f10ff79598c01c63b4eb50ffc74000cadbc0
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-02 19:24:39 +0530

    practice

--- REACTJS/react-revision/src/App.jsx
+++ REACTJS/react-revision/src/App.jsx
@@ -7,17 +7,54 @@ import Greeting from './Greeting'
 import Profile from './Profile'
 import Default from './Default'
 import Message from './Message'
+import Counter from './Counter'
+import Counter7 from './Counter7'
+import UserInfo from './UserInfo'
+import Fetch from './Fetch'
+import Counter11 from './Counter11'
+import Timer from './Timer'
+import Hello from './Hello'
+import Greeting1 from './Greeting1'
+import Counter15 from './Counter15'
+import Toggle from './Toggle'
+import { Component } from 'react'
+import Parent from './Parent'
+import Parent21 from './Parent21'
+import Parent22 from './Parent22'
+import Login from './Login'
+import UserList from './Uselist'
+import Form from './Form'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Welcome/>
-     <Greeting name="sree" /> 
-     <Profile name="bhavi" age={21} /> 
-     <Default/>
-     <Message/>
+     {/* <Welcome/> */}
+     {/* <Greeting name="sree" />  */}
+     {/* <Profile name="bhavi" age={21} />  */}
+     {/* <Default/> */}
+     {/* <Message count={3}/> */}
+     {/* <Counter/> */}
+     {/* <Counter7/> */}
+     {/* <UserInfo/> */}
+     {/* <Fetch/> */}
+     {/* <Counter11/> */}
+    {/* <Timer/> */}
+    {/* <Hello/> */}
+    {/* <Greeting1 name="sree"/> */}
+    {/* <Counter15/> */}
+    {/* <Toggle/> */}
+    17
+    18
+    19
+    <Parent/>
+    <Parent21/>
+    <Parent22/>
+    <Login/>
+    <UserList/>
+    <Form/>
+
     </>
   )
 }
