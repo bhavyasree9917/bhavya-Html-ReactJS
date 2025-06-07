d3ef0a49192897fdffcac926a8a6fa6b25a478d9
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-07 18:57:03 +0530

    practice

--- REACTJS/react-revision/src/Counter.jsx
+++ REACTJS/react-revision/src/Counter.jsx
@@ -22,7 +22,7 @@ class Counter extends Component {
             <div >
                 <h2>Counter App (Class Component)</h2>
                 <p>Count: {this.state.count}</p>
-                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.increment}></button>
                 <button onClick={this.decrement}>Decrement</button>
                 <button onClick={this.reset}>Reset</button>
             </div>
