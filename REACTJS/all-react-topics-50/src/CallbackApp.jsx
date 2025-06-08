1f77035ff5b7dfdd31980fb33ed8d165cc80b7d5
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-08 20:52:08 +0530

    practice

--- REACTJS/all-react-topics-50/src/CallbackApp.jsx
+++ REACTJS/all-react-topics-50/src/CallbackApp.jsx
@@ -11,7 +11,7 @@ export default function CallbackApp() {
   const theme = {
     backgroundColor: dark ? 'black' : 'white',
     color: dark ? 'white' : 'black',
-    padding: '10px',
+    padding: '15px',
   };
 
   return (
