ea51be4f2c951dacd4f5c063bc447717dc29f510
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-09 19:11:18 +0530

    react-revision

--- REACTJS/react-revision/src/SiblingB.jsx
+++ REACTJS/react-revision/src/SiblingB.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SiblingB({ receivedMessage }) {
   return (
     <div>
-      <h3>Sibling B</h3>
+      <h3>Siblingb</h3>
       
       <p>Received: {receivedMessage}</p>
     </div>
