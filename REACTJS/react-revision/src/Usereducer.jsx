24ec752237b15799f5d0ad514927f81cc1f45267
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-10 20:18:24 +0530

    react-revision

---
+++ REACTJS/react-revision/src/Usereducer.jsx
@@ -0,0 +1,27 @@
+// 28. useReducer for Complex State
+// useReducer is useful when managing complex state logic. Instead of using multiple useState calls, useReducer simplifies state transitions with a reducer function
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
+  );
+}
+// Here, dispatch is used to send actions (increment or decrement) to update the state.
