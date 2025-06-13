96dbb580a4553ca386b31ccd8c9a519c493cfba7
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-13 18:47:49 +0530

    react revision

--- REACTJS/react-revision/src/UserInfo.jsx
+++ REACTJS/react-revision/src/UserInfo.jsx
@@ -24,7 +24,7 @@ class UserInfo extends Component {
                 <p>Name: {this.state.name}</p>
                 <p>Age: {this.state.age}</p>
                 <button onClick={this.updateName}>name change</button>
-                <button onClick={this.updateAge}>Change Age</button>
+                <button onClick={this.updateAge}></button>
             </div>
         );
     }
