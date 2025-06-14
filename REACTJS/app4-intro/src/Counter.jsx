10fd29ad44c6a1a765cf36a394a548b68ba7e7e8
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-14 19:14:39 +0530

    revision

---
+++ REACTJS/app4-intro/src/Counter.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+export default class Counter extends Component{
+    constructor(){
+        super()
+        this.state={
+            count:0
+        }
+    }
+
+    increment=()=>{
+        this.setState({count:this.state.count+1})
+    }
+    render(){
+        return<div>
+            <h1>count:{this.state.count}</h1>
+            <button onClick={this.increment}>increment</button>
+        </div>
+    }
+
+}
