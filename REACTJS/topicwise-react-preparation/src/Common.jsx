4c76889e6ed73f41f926bcf46e68819be9b5141a
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-15 19:33:47 +0530

    topicwise-react-preparation

---
+++ REACTJS/topicwise-react-preparation/src/Common.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+export default class Common extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 10
+        }
+    }
+    increment = () => {
+        this.setState({ count: this.state.count + 10 })
+    }
+    decrement = () => {
+        this.setState({ count: this.state.count - 10 })
+
+    }
+    reset = () => {
+        this.setState({ count: 0 })
+
+    }
+    render() {
+        return <div>
+            {this.props.render(this.state.count, this.increment, this.decrement, this.reset)}
+        </div>
+    }
+}
