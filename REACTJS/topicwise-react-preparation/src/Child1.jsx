d51ffb835e1a4ab6364e714e4602f343c1786661
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-16 19:40:56 +0530

    topicwise-react-preparation

--- REACTJS/topicwise-react-preparation/src/Child1.jsx
+++ REACTJS/topicwise-react-preparation/src/Child1.jsx
@@ -5,7 +5,7 @@ render(){
     return<div>
         <h1>count:{this.props.count}</h1>
         <button onClick={this.props.increment}>increment</button>
-        <button onClick={this.props.decrement}>decrement</button>
+        <button onClick={this.props.decrement}>dec</button>
         <button onClick={this.props.reset}>reset</button>
     </div> 
 }
