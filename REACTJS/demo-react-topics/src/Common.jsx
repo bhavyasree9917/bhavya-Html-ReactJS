6e7c03b91477667f89cb89d71fbb7a6895195f72
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-18 16:12:59 +0530

    react-revision

---
+++ REACTJS/demo-react-topics/src/Common.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export default class Common extends Component{
+    constructor(){
+        super()
+        this.state={
+            count:0
+        }
+    }
+    render(){
+        return<div>
+            
+        </div>
+    }
+}
