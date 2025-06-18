e6648398f7d99a147ce0054537323376ca2ae890
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-18 16:13:05 +0530

    react-revision

---
+++ REACTJS/demo-react-topics/src/Parent.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import Common from "./Common";
+
+export default class Parent extends Component{
+    render(){
+        return<div>
+        
+    
+        </div>
+    }
+}
