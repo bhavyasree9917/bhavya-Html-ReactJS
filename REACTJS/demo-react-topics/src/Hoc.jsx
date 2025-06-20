fbecfdd2ff1726cd33b77b256c01d1f7a04b90e7
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-20 13:22:34 +0530

    hoc

--- REACTJS/demo-react-topics/src/Hoc.jsx
+++ REACTJS/demo-react-topics/src/Hoc.jsx
@@ -1,32 +1,34 @@
-import { Component } from "react";
-import Clickcount from "./Clickcount";
+import { Component } from "react"
 
 const Hoc = (Clickcount) => {
-    class Hoc extends Component {
+    class Ram extends Component {
         constructor() {
             super()
             this.state = {
                 count: 0
             }
-            increment = () => {
-                this.setState({ count: this.state.count + 1 })
-            }
-            decrement = () => {
-                this.setState({ count: this.state.count - 1 })
-            }
-            reset = () => {
-                this.setState({ count: 0 })
-            }
+        }
+        increment = () => {
+            this.setState({ count: this.state.count + 1 })
+        }
+        decrement = () => {
+            this.setState({ count: this.state.count - 1 })
+        }
+        reset = () => {
+            this.setState({ count: 0 })
         }
         render() {
+            const { increment, decrement, reset } = this
             return <div>
                 <Clickcount count={this.state.count}
-                    increment={this.increment}
-                    decrement={this.decrement}
-                    reset={this.reset} />
+                    increment={increment}
+                    decrement={decrement}
+                    reset={reset} />
+
             </div>
         }
     }
+    return Ram
 
 }
-export default Hoc;
+export default Hoc
