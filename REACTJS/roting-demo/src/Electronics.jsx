5ff9c2b99c14695910e7eb2e6f5f1ea1e6f66032
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-22 19:30:00 +0530

    routing

---
+++ REACTJS/roting-demo/src/Electronics.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Electronics extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return<div>
+            <h1>welcome to elctronics page</h1>
+        </div>
+    }
+}
