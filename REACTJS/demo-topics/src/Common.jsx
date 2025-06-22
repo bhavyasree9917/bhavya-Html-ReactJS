eb97e5cbf37d14e5b4d49218d6bb0a1d5c73e3cc
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-22 19:29:05 +0530

    routing

--- REACTJS/demo-topics/src/Common.jsx
+++ REACTJS/demo-topics/src/Common.jsx
@@ -1,24 +1,24 @@
 import { Component } from "react";
 
-export default class Common extends Component{
-    constructor(){
+export default class Common extends Component {
+    constructor() {
         super()
-        this.state={
-            count:0
+        this.state = {
+            count: 0
         }
     }
-    increment=()=>{
-        this.setState({count:this.state.count+1})
+    increment = () => {
+        this.setState({ count: this.state.count + 1 })
     }
-    decrement=()=>{
-        this.setState({count:this.state.count-1})
+    decrement = () => {
+        this.setState({ count: this.state.count - 1 })
     }
-    reset=()=>{
-        this.setState({count:0})
+    reset = () => {
+        this.setState({ count: 0 })
     }
-    render(){
-        return<div>
-{this.props.render(this.state.count,this.increment,this.decrement,this.reset)}
+    render() {
+        return <div>
+            {this.props.render(this.state.count, this.increment, this.decrement, this.reset)}
         </div>
     }
 }
