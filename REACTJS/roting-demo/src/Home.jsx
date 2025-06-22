c09954a5686571f35fa93e4299d5bffcb3aa2bed
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-22 19:30:14 +0530

    routing

---
+++ REACTJS/roting-demo/src/Home.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Home extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return<div>
+            <h1>welcome to home page</h1>
+        </div>
+    }
+}
