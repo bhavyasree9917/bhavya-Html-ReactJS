0d33eb53c8c5f0add19872223b157f714919c648
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-24 20:34:01 +0530

    spa

--- REACTJS/single-page-project/src/Dashbaoard.jsx
+++ REACTJS/single-page-project/src/Dashbaoard.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-      <h2>Welcome to Dashboard Page !!</h2>
+      <h2>Welcome to Dashboard Page !!!!</h2>
     </div>
   )
 }
-
 export default Dashboard
