e0be71d166d0c7eacf8a4bf37e2619ceab6e000d
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-24 20:32:53 +0530

    spa

--- REACTJS/single-page-project/src/App.jsx
+++ REACTJS/single-page-project/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Register from './Register'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const Register = lazy(() => import("./Register"));
+const Login = lazy(() => import("./Login"));
+const Dashboard = lazy(() => import("./Dashbaoard"));
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <Register />
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
