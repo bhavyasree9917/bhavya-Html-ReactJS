771707e8e0639dac66455dd5bcec4398aba8969b
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-27 18:10:17 +0530

    react-revision

--- REACTJS/react-simple-project/src/Dashboard2.jsx
+++ REACTJS/react-simple-project/src/Dashboard2.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard2 = () => {
   return (
     <div>
-      <h2>Welcome to Dashboard2 P</h2>
+      <h2>Welcome to Dashboard2 Page!</h2>
     </div>
   );
 };
