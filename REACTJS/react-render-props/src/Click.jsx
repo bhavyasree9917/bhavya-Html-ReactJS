f327c43de3eac5fb2284c66be4b0d948a2facee6
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-06-30 19:41:31 +0530

    react

--- REACTJS/react-render-props/src/Click.jsx
+++ REACTJS/react-render-props/src/Click.jsx
@@ -4,7 +4,7 @@ export default  class Click extends Component{
     render(){
         return(<div>
             <h1>{this.props.count}</h1>
-            <button onClick={this.props.increment}>increment</button>
+            <button onClick={this.props.increment}></button>
             <button onClick={this.props.decrement}></button>
             <button onClick={this.props.reset}></button>
         </div>)
