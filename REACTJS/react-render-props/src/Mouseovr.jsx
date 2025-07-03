f2f7a2ec42556c6f38f824011d12bcb7de356571
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-03 18:53:52 +0530

    practice

--- REACTJS/react-render-props/src/Mouseovr.jsx
+++ REACTJS/react-render-props/src/Mouseovr.jsx
@@ -6,7 +6,7 @@ export default  class Mouseovr extends Component{
            
             <h1>{this.props.count}</h1>
             <button onMouseOver={this.props.increment}>increment</button>
-            <button onMouseOver={this.props.decrement}>decrement</button>
+            <button onMouseOver={this.props.decrement}>decrement here</button>
             <button onMouseOver={this.props.reset}>reset</button>
         </div>)
     }
