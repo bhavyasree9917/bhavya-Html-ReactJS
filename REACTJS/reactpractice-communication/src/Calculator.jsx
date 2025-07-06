eb67e87f0c43f36f7788907063508c4372d751df
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-06 18:49:02 +0530

    react

--- REACTJS/reactpractice-communication/src/Calculator.jsx
+++ REACTJS/reactpractice-communication/src/Calculator.jsx
@@ -6,7 +6,7 @@ class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 12
+      number: 15
     };
   }
 
@@ -21,7 +21,7 @@ class Calculator extends Component {
         <Result number={this.state.number} />
         <InputNumber value={this.state.number} onNumberChange={this.handleNumberChange} />
         {/* <Result number={this.state.number} /> */}
-        
+
       </div>
     );
   }
