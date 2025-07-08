1652cbcac587814b505cbc546dedb64250345cee
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-08 20:08:03 +0530

    practice

--- REACTJS/reactpractice-communication/src/DisplayChild.jsx
+++ REACTJS/reactpractice-communication/src/DisplayChild.jsx
@@ -2,7 +2,7 @@ const DisplayChild = ({ handleChange }) => {
     return (
         <input
             type="text"
-            placeholder="Type "
+            placeholder="Type something..."
             onChange={handleChange}
         />
     );
