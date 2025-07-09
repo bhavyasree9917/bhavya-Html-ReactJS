9e46b5ba444b00ff6ca13f09e94c4670648708bf
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-09 20:03:32 +0530

    react

--- REACTJS/reactpractice-communication/src/GrandParent.jsx
+++ REACTJS/reactpractice-communication/src/GrandParent.jsx
@@ -5,7 +5,7 @@ import Parnt from './Parnt';
 
 class Grandparent extends Component {
   render() {
-    const theme = 'ligh'; 
+    const theme = 'light'; 
     return (
       <div>
         <h2>Grandparent Component</h2>
