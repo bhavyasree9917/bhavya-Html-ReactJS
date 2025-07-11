c8415c8324316ceffe8b72f7af2cf4bc17a1fc31
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-11 18:59:19 +0530

    practice

---
+++ REACTJS/reactpractice-communication/src/MouseTracker.jsx
@@ -0,0 +1,21 @@
+// MouseTracker.js
+import React, { useState } from 'react';
+
+const MouseTracker = ({ render }) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setPosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  return (
+    <div style={{ height: '100vh' }} onMouseMove={handleMouseMove}>
+      {render(position)}
+    </div>
+  );
+};
+
+export default MouseTracker;
