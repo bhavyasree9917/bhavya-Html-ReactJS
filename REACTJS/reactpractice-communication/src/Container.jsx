fe56ac27eb069ddda5df905a8ad1ca1db1982bd0
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-17 19:28:30 +0530

    React

--- REACTJS/reactpractice-communication/src/Container.jsx
+++ REACTJS/reactpractice-communication/src/Container.jsx
@@ -6,7 +6,7 @@ const Container = () => {
     const [inputtext, settext] = useState("");
 
     const display = () => {
-        settext("bhavya sr");
+        settext("bhavya sre");
     };
 
     const handleChange = (e) => {
