df7b8f48ac47df789bf70f07b7de972c141088c5
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-19 18:21:29 +0530

    React

--- Reactjs-projects/calender-appointment/src/components/AppointmentForm.jsx
+++ Reactjs-projects/calender-appointment/src/components/AppointmentForm.jsx
@@ -30,6 +30,7 @@ const AppointmentForm = ({ editData, setEditData }) => {
     }
     setForm({ eventName: '', date: '', time: '', location: '', description: '', participants: '' });
   };
+  
   return (
     <form onSubmit={handleSubmit} className="p-4 border rounded mb-4">
       <input name="eventName" placeholder="Event Name" value={form.eventName} onChange={handleChange} required className="block w-full mb-2 border p-2" />
