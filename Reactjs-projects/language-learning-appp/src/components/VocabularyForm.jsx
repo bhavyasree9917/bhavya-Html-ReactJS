c49b7f696f3d10be57be1a98f12fddc0c5b5e993
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-07-20 19:12:39 +0530

    practice

--- Reactjs-projects/language-learning-appp/src/components/VocabularyForm.jsx
+++ Reactjs-projects/language-learning-appp/src/components/VocabularyForm.jsx
@@ -29,7 +29,7 @@ const VocabularyForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="p-4 border rounded mb-4">
+    <form onSubmit={handleSubmit} className="p-4  rounded mb-4">
       <input
         name="word"
         placeholder="Word"
