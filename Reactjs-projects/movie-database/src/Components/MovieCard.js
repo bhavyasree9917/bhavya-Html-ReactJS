fc244ef315b4f8d7a002d15f72ee053f862cbfbe
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-03 19:36:13 +0530

    Practice

--- Reactjs-projects/movie-database/src/Components/MovieCard.js
+++ Reactjs-projects/movie-database/src/Components/MovieCard.js
@@ -6,7 +6,7 @@ const MovieCard = ({ movie, removeMovie, addToWatchList }) => {
     <div className="movie-card">
       <h3>{movie.title}</h3>
       <p><strong>Director-</strong> {movie.director}</p>
-      <p><strong>Year</strong> {movie.releaseYear}</p>
+      <p><strong>Year-</strong> {movie.releaseYear}</p>
       <p><strong>Genre</strong> {movie.genre}</p>
       <p><strong>Rating</strong> {movie.rating}</p>
       <p><strong>Actors</strong> {movie.actors.join(', ')}</p>
