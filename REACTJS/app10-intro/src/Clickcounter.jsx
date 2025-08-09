92305db6daa82fbc19c400dd193fae2bb2391527
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-09 19:29:16 +0530

    Practice

--- REACTJS/app10-intro/src/Clickcounter.jsx
+++ REACTJS/app10-intro/src/Clickcounter.jsx
@@ -10,7 +10,7 @@ const Clickcounter = () => {
 
     return <div>
         <h1>count{count}</h1>
-        <button onClick={handleclick}>increment  </button>
+        <button onClick={handleclick}>incremen  </button>
 
     </div>
 
