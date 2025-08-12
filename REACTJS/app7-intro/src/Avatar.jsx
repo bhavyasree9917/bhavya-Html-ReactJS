785624594f1ed80b9d564d67f10b142670a051f8
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-12 20:10:02 +0530

    React

--- REACTJS/app7-intro/src/Avatar.jsx
+++ REACTJS/app7-intro/src/Avatar.jsx
@@ -2,8 +2,7 @@ import React from "react";
 const Avatar=({imageurl})=>{
     return<div>
 <img src="{imageurl}"/>   
-    </div>
-   
+    </div> 
 }
 Avatar.defaultProps={
     imageurl:'https://via.placeholder.com/150'
