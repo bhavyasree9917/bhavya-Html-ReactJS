a3524bb349e4286b610280d0d015562524b38ceb
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-15 20:06:02 +0530

    Pract

--- REACTJS/app4-intro/src/TodoAPP.jsx
+++ REACTJS/app4-intro/src/TodoAPP.jsx
@@ -9,7 +9,7 @@ function TodoApp() {
     }
 
     return <div>
-        <h1>welcome to my to </h1>
+        <h1>welcome to my t </h1>
         <TodoForm addtodo={addtodo} />
         <TodoList todos={todos} />
 
