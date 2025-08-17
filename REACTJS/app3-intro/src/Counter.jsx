652812d2816a6c39f080aca3f2fb3ff8d8c2e71e
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-17 20:08:42 +0530

    React

--- REACTJS/app3-intro/src/Counter.jsx
+++ REACTJS/app3-intro/src/Counter.jsx
@@ -13,7 +13,7 @@ export default class Counter extends Component {
     render() {
         return <div>
             <h1>{this.state.count}</h1>
-            <button onClick={this.increment}>increase the </button>
+            <button onClick={this.increment}>increase th </button>
         </div>
     }
 
