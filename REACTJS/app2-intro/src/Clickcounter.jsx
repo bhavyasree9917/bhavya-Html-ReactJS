d98e791eaefc8250e7ccaf9c9f51f11a97b8a41c
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-18 21:53:00 +0530

    React

--- REACTJS/app2-intro/src/Clickcounter.jsx
+++ REACTJS/app2-intro/src/Clickcounter.jsx
@@ -7,7 +7,7 @@ const Clickcounter = () => {
 
     return <div>
         <h1>count:{count}</h1>
-        <button onClick={handleclick}>clickhere</button>
+        <button onClick={handleclick}>clickher</button>
 
     </div>
 }
