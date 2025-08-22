d8d73836749b24b0ba357e27356c01f2b3d0717d
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-22 20:08:14 +0530

    React

--- REACTJS/all-react-topics-50/src/FocusInput.jsx
+++ REACTJS/all-react-topics-50/src/FocusInput.jsx
@@ -10,7 +10,7 @@ function FocusInput() {
   return (
     <div>
       <input ref={inputRef} type="text" placeholder="Click button to focus me" />
-      <button onClick={handleClick}>Fo </button>
+      <button onClick={handleClick}>F </button>
       
     </div>
   );
