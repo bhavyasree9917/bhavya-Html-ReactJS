02f769dab2b783b96a663dc943ba57e19073b383
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-24 19:24:54 +0530

    React

--- REACTJS/all-react-topics-50/src/FetchStatus.jsx
+++ REACTJS/all-react-topics-50/src/FetchStatus.jsx
@@ -23,6 +23,7 @@ export default function FetchStatus() {
   }, []);
 
   if (loading) return <div>loading</div>;
+  
   if (error) return <div>Error {error}</div>;
 
   return <div>{data.title}</div>;
