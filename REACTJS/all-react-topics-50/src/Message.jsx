eb46b58e642c36e01c950eb336ffd39768c7b183
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-25 19:35:42 +0530

    Pract

--- REACTJS/all-react-topics-50/src/Message.jsx
+++ REACTJS/all-react-topics-50/src/Message.jsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 class Message extends Component {
   render() {
-    return <p>You have {this.props.count} new mes</p>;
+    return <p>You have {this.props.count} new me</p>;
   }
 }
 export default Message
