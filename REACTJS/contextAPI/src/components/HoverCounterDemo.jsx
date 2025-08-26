b7e5c1c226215378d05d4cea592da8fad8bb2210
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-26 21:09:10 +0530

    React

--- REACTJS/contextAPI/src/components/HoverCounterDemo.jsx
+++ REACTJS/contextAPI/src/components/HoverCounterDemo.jsx
@@ -9,7 +9,7 @@ class HoverCounterDemo extends Component {
         <h2>The Count : {count}</h2>
         <button onMouseOver={handleIncrement}>Increment </button>
         <button onMouseOver={handleDecrement}>Decrement</button>
-        <button onMouseOver={handleReset}>Reset </button>
+        <button onMouseOver={handleReset}>Reset. </button>
       </>
     ); 
     
