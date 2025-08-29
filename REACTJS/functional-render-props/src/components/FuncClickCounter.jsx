b558a32c70be13017304ccaaf59ba49bb0c9cffb
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-29 19:46:00 +0530

    React

--- REACTJS/functional-render-props/src/components/FuncClickCounter.jsx
+++ REACTJS/functional-render-props/src/components/FuncClickCounter.jsx
@@ -10,7 +10,7 @@ const FuncClickCounter = ({
     <div>
       <button onClick={handleIncrement}>Increment operator</button>
       <button onClick={handleDecrement}>decrement operator </button>
-      <button onClick={handleReset}>reset</button>
+      <button onClick={handleReset}>reset </button>
       <h2>{count}</h2>
     </div>
     
