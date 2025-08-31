2880b0fd2210c3a8931998a35f188609160005e8
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-08-31 18:42:58 +0530

    Hoc

--- REACTJS/react-highder-order-component/src/App.jsx
+++ REACTJS/react-highder-order-component/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-           {count}
+          count is {count}
         </button>
         <p>
           edit <code>src/App.jsx</code>
