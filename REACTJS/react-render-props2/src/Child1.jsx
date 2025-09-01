8140e1c036f7cefd1188147bc10e3250962d2f43
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-09-01 20:34:38 +0530

    PRACT

--- REACTJS/react-render-props2/src/Child1.jsx
+++ REACTJS/react-render-props2/src/Child1.jsx
@@ -7,7 +7,7 @@ export default class Child1 extends Component {
     render() {
         return <div>
             <h1>{this.props.count}</h1>
-            <button onClick={this.props.increment}>increment count</button>
+            <button onClick={this.props.increment}>increment </button>
             <button onClick={this.props.decrement}>decrement  </button>
             <button onClick={this.props.reset}>reset</button>
         </div>
