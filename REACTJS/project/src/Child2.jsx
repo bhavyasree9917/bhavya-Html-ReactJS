f45d4735738e6da33c848d3d7ba4fbc41a3fcd45
bhavyasree9917 <bhavyasree9917@gmail.com>
2025-09-12 20:25:04 +0530

    project

--- REACTJS/project/src/Child2.jsx
+++ REACTJS/project/src/Child2.jsx
@@ -4,7 +4,7 @@ import Child3 from "./Child3";
 export default class extends Component{
     render(){
         return (
-        <div><h1>welcome Child2</h1>
+        <div><h1>welcome</h1>
         
             <Child3 msg={this.props.msg}/></div>)
     }
